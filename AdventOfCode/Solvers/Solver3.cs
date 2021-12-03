093e4a992b91130832ffed0ad7b2fcf437134dd0
Mathieu Fuchs <mathieufuchs13@gmail.com>
2021-12-03 08:56:23 +0100

    Solved day-3

--- AdventOfCode/Solvers/Solver3.cs
+++ AdventOfCode/Solvers/Solver3.cs
@@ -1,4 +1,6 @@
-﻿namespace AdventOfCode
+﻿using System.Collections;
+
+namespace AdventOfCode
 {
     public class Solver3 : ISolver
     {
@@ -7,11 +9,84 @@
         }
         public async Task<string> Solve1(string input)
         {
-            throw new NotImplementedException();
+            var rows = input.GetRows();
+
+            var chars = rows.Select(r => r.ReplaceLineEndings("").ToArray());
+
+            var rowLength = chars.First().Length;
+
+            var gammaRateBits = "";
+            var epsilonRateBits = "";
+
+            for(var i = 0; i < rowLength; i++)
+            {
+                var ones = chars.Count(c => c.ElementAt(i) == '1');
+                var zeros = chars.Count(c => c.ElementAt(i) == '0');
+                var mostOnes = ones > zeros;
+                gammaRateBits += mostOnes ? "1": "0";
+                epsilonRateBits += mostOnes ? "0" : "1";
+            }
+
+            var gammaRate = Convert.ToInt32(gammaRateBits, 2);
+            var epsilonRate = Convert.ToInt32(epsilonRateBits, 2);
+
+            var result = epsilonRate * gammaRate;
+            return result.ToString();
         }
+
         public async Task<string> Solve2(string input)
         {
-            throw new NotImplementedException();
+            var rows = input.GetRows();
+
+            var chars = rows.Select(r => r.ReplaceLineEndings("").ToArray());
+
+            var rowLength = chars.First().Length;
+
+            var seq = "";
+            var oxygen = "";
+            var scrubber = "";
+
+            var filtered = chars;
+
+            for (var i = 0; i < rowLength; i++)
+            {
+                var ones = filtered.Count(c => c.ElementAt(i) == '1');
+                var zeros = filtered.Count(c => c.ElementAt(i) == '0');
+                var mostOnes = ones >= zeros;
+                seq += mostOnes ? "1": "0";
+
+                filtered = chars.Where(c => new string(c).StartsWith(seq));
+                if (filtered.Count() == 1)
+                {
+                    oxygen = new string(filtered.First());
+                    break;
+                }
+            }
+
+            filtered = chars;
+            seq = "";
+
+            for (var i = 0; i < rowLength; i++)
+            {
+
+                var ones = filtered.Count(c => c.ElementAt(i) == '1');
+                var zeros = filtered.Count(c => c.ElementAt(i) == '0');
+                var mostOnes = ones >= zeros;
+                seq += mostOnes ? "0" : "1";
+
+                filtered = chars.Where(c => new string(c).StartsWith(seq));
+                if (filtered.Count() == 1)
+                {
+                    scrubber = new string(filtered.First());
+                    break;
+                }
+            }
+
+            var gammaRate = Convert.ToInt32(oxygen, 2);
+            var epsilonRate = Convert.ToInt32(scrubber, 2);
+
+            var result = epsilonRate * gammaRate;
+            return result.ToString();
         }
     }
 }
