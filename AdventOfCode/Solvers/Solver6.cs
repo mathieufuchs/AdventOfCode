b73cece8e3c6b2a3e53022229596032487ae2f26
Mathieu Fuchs <mathieufuchs13@gmail.com>
2021-12-06 21:12:31 +0100

    Added day 6

--- AdventOfCode/Solvers/Solver6.cs
+++ AdventOfCode/Solvers/Solver6.cs
@@ -7,11 +7,56 @@
         }
         public async Task<string> Solve1(string input)
         {
-            throw new NotImplementedException();
+            //input = UseDummyInput();
+
+            var rows = input.Split(",").Select(r => int.Parse(r)).ToList();
+
+            for(var i = 0; i < 80; i++)
+            {
+                var length = rows.Count;
+                for(var j = 0; j < length; j++)
+                {
+                    if(rows[j] == 0)
+                    {
+                        rows[j] = 6;
+                        rows.Add(8);
+                    }
+                    else
+                    {
+                        rows[j]--;
+                    }
+                }
+            }
+
+            return rows.Count.ToString();
         }
         public async Task<string> Solve2(string input)
         {
-            throw new NotImplementedException();
+            var rows = input.Split(",").Select(r => int.Parse(r)).ToList();
+
+            for (var i = 0; i < 256; i++)
+            {
+                var length = rows.Count;
+                for (var j = 0; j < length; j++)
+                {
+                    if (rows[j] == 0)
+                    {
+                        rows[j] = 6;
+                        rows.Add(8);
+                    }
+                    else
+                    {
+                        rows[j]--;
+                    }
+                }
+            }
+
+            return rows.Count.ToString();
+        }
+
+        private string UseDummyInput()
+        {
+            return "3,4,3,1,2";
         }
     }
 }
