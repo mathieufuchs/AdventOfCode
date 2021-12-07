ec6a339783304f1a9ad3f7c118fedbbebcf20f70
Mathieu Fuchs <mathieufuchs13@gmail.com>
2021-12-07 19:27:36 +0100

    Day 7 solution

--- AdventOfCode/Solvers/Solver7.cs
+++ AdventOfCode/Solvers/Solver7.cs
@@ -7,11 +7,70 @@
         }
         public async Task<string> Solve1(string input)
         {
-            throw new NotImplementedException();
+            var result = Internal_Solve_1(input);
+            return result.ToString();
         }
+
+        public static int Internal_Solve_1(string input)
+        {
+            var crabPositions = input.Split(',').Select(m => int.Parse(m)).ToArray();
+
+            var fuel = int.MaxValue;
+
+            var posMin = crabPositions.Min();
+            var posMax = crabPositions.Max();
+
+            for (var targetPos = posMin; targetPos <= posMax; targetPos++)
+            {
+                var tempFuel = 0; 
+
+                foreach(var crabPosition in crabPositions)
+                {
+                    tempFuel += Math.Abs(crabPosition - targetPos);
+                }
+
+                if(tempFuel < fuel)
+                {
+                    fuel = tempFuel;
+                }
+            }
+
+            return fuel;
+        }
+
+        public static int Internal_Solve_2(string input)
+        {
+            var crabPositions = input.Split(',').Select(m => int.Parse(m)).ToArray();
+
+            var fuel = int.MaxValue;
+
+            var posMin = crabPositions.Min();
+            var posMax = crabPositions.Max();
+
+            for (var targetPos = posMin; targetPos <= posMax; targetPos++)
+            {
+                int tempFuel = 0;
+
+                foreach (var crabPosition in crabPositions)
+                {
+                    int steps = Math.Abs(crabPosition - targetPos);
+                    // tempFuel += Enumerable.Range(1, steps).Sum();        => this works but is much slower than the mathematical form below :) 
+                    tempFuel += steps * (steps + 1) / 2;
+                }
+
+                if (tempFuel < fuel)
+                {
+                    fuel = tempFuel;
+                }
+            }
+
+            return fuel;
+        }
+
         public async Task<string> Solve2(string input)
         {
-            throw new NotImplementedException();
+            var result = Internal_Solve_2(input);
+            return result.ToString();
         }
     }
 }
--- AdventOfCodeTests/Solvers/Solver7Tests.cs
+++ AdventOfCodeTests/Solvers/Solver7Tests.cs
@@ -12,9 +12,32 @@ namespace AdventOfCode.Tests
     public class Solver7Tests
     {
         [TestMethod()]
-        public void Solve1Test()
+        public void Solve()
         {
-            Assert.Fail();
+            var input = "16,1,2,0,4,2,7,1,2,14";
+            var expected = 37;
+
+            var result = Solver7.Internal_Solve_1(input);
+
+            Assert.AreEqual(expected, result);
         }
+
+        [TestMethod()]
+        public void Solve_2()
+        {
+            var input = "16,1,2,0,4,2,7,1,2,14";
+            var expected = 168;
+
+            var result = Solver7.Internal_Solve_2(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+
+        [TestMethod]
+        public async Task Solve1() => await SolverTestHelper.Solve(7, 1, "357353");
+
+        [TestMethod]
+        public async Task Solve2() => await SolverTestHelper.Solve(7, 2, "104822130");
     }
 }
